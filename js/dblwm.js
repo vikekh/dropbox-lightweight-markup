e04b8d4da5286610b224d2a21e41cee462f352f9
vieekk <vieekk@gmail.com>
2013-09-13 19:03:38 +0200

    Added support for Textile.

--- js/dblwm.js
+++ js/dblwm.js
@@ -1,11 +1,18 @@
 jQuery(document).ready(function ($) {
-	var $title = $('title').text('. . .');
-	var md = $.url().param('md');
+	$.getJSON('config.json', function (config) {
+		var $title = $('title').text('. . .');
+		var $target = $('#target');
 
-	if (md !== undefined) {
-		$.get('md/' + md + '.md', function (data) {
-			$('#markdown').append(markdown.toHTML(data));
-			$title.text($('h1').first().text());
-		});
-	}
+		if ($.url().param(config.get.markdown) !== undefined) {
+			$.get(config.root.markdown + $.url().param(config.get.markdown) + '.md', function (data) {
+				$target.append(markdown.toHTML(data));
+			});
+		} else if ($.url().param(config.get.textile) !== undefined) {
+			$.get(config.root.textile + $.url().param(config.get.textile) + '.textile', function (data) {
+				$target.append(textile.parse(data));
+			});
+		}
+
+		$title.text($('h1').first().text());
+	});
 });
---
+++ markdown/sample.md
@@ -0,0 +1,4 @@
+Markdown Sample
+===============
+
+If there's anything worse than a bore it's a boring whore.
