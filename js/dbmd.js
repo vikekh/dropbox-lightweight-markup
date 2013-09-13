8ec0cde3169c3b0408d39ba8deb2f3cd99b7dfe7
vieekk <vieekk@gmail.com>
2013-09-13 02:43:12 +0200

    The title of the page is now collected from the first header.

--- js/dbmd.js
+++ js/dbmd.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function ($) {
+	var $title = $('title').text('. . .');
 	var md = $.url().param('md');
 
 	if (md !== undefined) {
 		$.get('md/' + md + '.md', function (data) {
 			$('#markdown').append(markdown.toHTML(data));
+			$title.text($('h1').first().text());
 		});
 	}
 });
