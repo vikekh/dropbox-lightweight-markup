820dd48862b5bec1efb3b0430ed104e4b3372f25
vieekk <vieekk@gmail.com>
2013-09-13 19:05:20 +0200

    Fixed broken title update.

--- js/dblwm.js
+++ js/dblwm.js
@@ -6,13 +6,13 @@ jQuery(document).ready(function ($) {
 		if ($.url().param(config.get.markdown) !== undefined) {
 			$.get(config.root.markdown + $.url().param(config.get.markdown) + '.md', function (data) {
 				$target.append(markdown.toHTML(data));
+				$title.text($('h1').first().text());
 			});
 		} else if ($.url().param(config.get.textile) !== undefined) {
 			$.get(config.root.textile + $.url().param(config.get.textile) + '.textile', function (data) {
 				$target.append(textile.parse(data));
+				$title.text($('h1').first().text());
 			});
 		}
-
-		$title.text($('h1').first().text());
 	});
 });
